2ddb5dfe431a63c78d33a52efe70eac5768fad43
hashchange <heim@zeilenwechsel.de>
2015-08-20 17:39:33 +0200

    Changed demo directory structure

--- demo/amd/amd.js
+++ demo/amd/amd.js
@@ -1,16 +1,4 @@
-requirejs.config( {
-
-    baseUrl: '../../bower_components',
-
-    paths: {
-        'jquery': '../demo/bower_demo_components/jquery/dist/jquery',
-        'underscore': 'underscore/underscore',
-        'backbone': 'backbone/backbone',
-        'backbone.select': 'backbone.select/dist/amd/backbone.select',
-        'backbone.cycle': '/dist/amd/backbone.cycle'
-    }
-
-} );
+// amd.js
 
 require( [
 
---
+++ demo/amd/require-config.js
@@ -0,0 +1,13 @@
+requirejs.config( {
+
+    baseUrl: '../../bower_components',
+
+    paths: {
+        'jquery': '../demo/bower_demo_components/jquery/dist/jquery',
+        'underscore': 'underscore/underscore',
+        'backbone': 'backbone/backbone',
+        'backbone.select': 'backbone.select/dist/amd/backbone.select',
+        'backbone.cycle': '/dist/amd/backbone.cycle'
+    }
+
+} );
