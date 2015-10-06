6bceee89f0e68169b7d997ec0c8489ab8ec5cbf9
hashchange <heim@zeilenwechsel.de>
2015-10-06 17:47:00 +0200

    Updated demo

--- demo/amd/jsbin/require-config.js
+++ demo/amd/jsbin/require-config.js
@@ -4,8 +4,8 @@ requirejs.config( {
         'jquery': 'https://code.jquery.com/jquery-1.11.3',
         'underscore': 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore',
         'backbone': 'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.1/backbone',
-        'backbone.select': 'https://cdn.rawgit.com/hashchange/backbone.select/1.3.2/dist/amd/backbone.select',
-        'backbone.cycle': 'https://cdn.rawgit.com/hashchange/backbone.cycle/2.0.2/dist/amd/backbone.cycle'
+        'backbone.select': 'https://cdn.rawgit.com/hashchange/backbone.select/1.5.0/dist/amd/backbone.select',
+        'backbone.cycle': 'https://cdn.rawgit.com/hashchange/backbone.cycle/2.1.0/dist/amd/backbone.cycle'
     }
 
 } );
